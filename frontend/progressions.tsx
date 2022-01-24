8d2dd97ac95a92cd66939b75c070cc20f5765c39
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-24 22:46:52 +0100

    feature: add progression chord inversions

--- frontend/progressions.tsx
+++ frontend/progressions.tsx
@@ -1,5 +1,6 @@
-import { h } from "preact";
+import { h, Fragment } from "preact";
 
+import { useToggle } from "./use-toggle";
 import { Note, NoteType } from "./note";
 
 import { MajorProgression, MinorProgression } from "./progression";
@@ -35,9 +36,75 @@ export function Progressions(props: { selectedNote: NoteType }) {
               <small data-fs="14">{progression.name()}</small>
 
               <div data-display="flex" data-direction="column" data-mt="24">
-                {progression.chords.map((chord) => (
-                  <strong data-mb="12">{chord.name()}</strong>
-                ))}
+                {progression.chords.map((chord) => {
+                  const details = useToggle();
+
+                  return (
+                    <Fragment>
+                      <div data-mb="6">
+                        <button
+                          class="c-button"
+                          onClick={details.toggle}
+                          data-variant="bare"
+                          data-mr="12"
+                        >
+                          {details.off && (
+                            <img
+                              height="12"
+                              width="12"
+                              loading="eager"
+                              src="/static/arrow-down-icon.svg"
+                              alt=""
+                            />
+                          )}
+
+                          {details.on && (
+                            <img
+                              height="12"
+                              width="12"
+                              loading="eager"
+                              src="/static/arrow-up-icon.svg"
+                              alt=""
+                            />
+                          )}
+                        </button>
+
+                        <strong>{chord.name()}</strong>
+                      </div>
+
+                      {details.on && (
+                        <ul
+                          style="list-style: none;"
+                          data-mb="12"
+                          data-pb="6"
+                          data-bcb="gray-300"
+                          data-bwb="2"
+                        >
+                          <li>
+                            <small>Root position</small>
+                            {chord.notes.map((note) => (
+                              <strong data-mr="6">{note.format()}</strong>
+                            ))}
+                          </li>
+
+                          <li data-mt="6">
+                            <small>First inversion</small>
+                            {chord.firstInversion().map((note) => (
+                              <strong data-mr="6">{note.format()}</strong>
+                            ))}
+                          </li>
+
+                          <li data-mt="6">
+                            <small>Second inversion</small>
+                            {chord.secondInversion().map((note) => (
+                              <strong data-mr="6">{note.format()}</strong>
+                            ))}
+                          </li>
+                        </ul>
+                      )}
+                    </Fragment>
+                  );
+                })}
               </div>
             </li>
           );
