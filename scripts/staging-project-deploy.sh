4257bd04037dd6c1277dd3b2f303d954834e46ba
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-06-17 19:33:11 +0200

    step: bump bgord-scripts

--- scripts/staging-project-deploy.sh
+++ scripts/staging-project-deploy.sh
@@ -3,10 +3,12 @@
 # Preload base bash configuration and functions
 source bgord-scripts/base.sh
 
+PROJECT_NAME=$(basename `git rev-parse --show-toplevel`)
+
 OUT_DIR="build"
 
 validate_non_empty $OUT_DIR "OUT_DIR"
 
-rsync -azP --delete build/ staging:/var/www/cordian
+rsync -azP --delete build/ staging:/var/www/$PROJECT_NAME
 
 success "Project deployed correctly!"
